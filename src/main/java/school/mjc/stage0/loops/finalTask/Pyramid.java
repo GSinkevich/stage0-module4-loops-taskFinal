0123cdf164daee42d8f703a266371dfa34b253ba
DESKTOP-GV962H6\sinke <sinkevich.work@gmail.com>
2022-12-06 21:49:23 +0300

    pyramid done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,8 +2,25 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int currentNumber = cathetusLength;
+        for (int i = 0; i < cathetusLength ; i++) {
+            for (int j = 0; j < cathetusLength+i; j++) {
+                if (j >= cathetusLength-i-1 && j <= cathetusLength+i-1 ){
+                    if(currentNumber-j >= 1){
+                        System.out.print(currentNumber-j);
+                    }
+                    else{
+                        System.out.print(j-currentNumber+2);
+                    }
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
     }
+
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
